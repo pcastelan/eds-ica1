7395777393e904b5b568bab43ffe205e8f9e2e8d
Paula Castelan Firme <paulacastelanf@gmail.com>
2017-03-20 05:13:51 +0000

    pequenas alterações

--- clinica.c
+++ clinica.c
@@ -5,19 +5,36 @@
 		+ 2 - menu do paciente
 
 */
+int usuario=0;
+int nivel=0;
+
 int opcao();
 int menus(int);
+int verificaCadastro(char*);
+void cadastro();
 
-int main(){
-	int usuario = menus(1);
+int main(){	
+	char *cpf;
+	int opcao;
+	usuario = menus(1);
+
 	if (usuario == 3){
 		printf("%s\n", "O sistema está sendo encerrado");
 	}
-	if (usuario == 1){
 
+	while (usuario != 3){
+
+		while (usuario == 1){
+
+			if(verificaCadastro(cpf) == 0){
+				cadastro();
+
+			} else {
+			
+			}
+		}
 	}
-
-
+	
 	return 0;
 }
 int opcao(){
@@ -26,8 +43,8 @@ int opcao(){
 	return opcao;
 }
 int menus(int tipo){
-		switch(tipo){
-		case 1: 	
+	switch (tipo){
+		case 1:
 			printf("%s\n", "BEM VINDO A CLINICA MÉDICA BEM ESTAR\n1 - Paciente\n2 - Administrador\n3 - Sair do Sistema");
 			return opcao();
 			break;
@@ -36,4 +53,12 @@ int menus(int tipo){
 			return opcao();
 			break;
 	}
+}
+
+int verificaCadastro(char *cpf){
+
+}
+
+void cadastro(){
+
 }
